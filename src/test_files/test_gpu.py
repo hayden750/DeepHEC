3844b09d365ce2c1f500e6140f4fe3a5049aa0d5
hayden750 <35261632+hayden750@users.noreply.github.com>
2021-01-25 10:44:08 +0000

    Added working ppo pendulum

---
+++ src/test_files/test_gpu.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+from packaging import version
+
+tf.debugging.set_log_device_placement(True)
+print(tf.config.experimental.list_physical_devices())
+
+########################################
+# check tensorflow version
+print("Tensorflow Version: ", tf.__version__)
+assert version.parse(tf.__version__).release[0] >= 2, \
+    "This program requires Tensorflow 2.0 or above"
+#######################################
+
+######################################
+# avoid CUDNN_STATUS_INTERNAL_ERROR
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+    except RuntimeError as e:
+        print(e)
+################################################
+device_name = tf.test.gpu_device_name()
+if device_name != '/device:GPU:0':
+    raise SystemError('GPU device not found')
+print('Found GPU at: {}'.format(device_name))
