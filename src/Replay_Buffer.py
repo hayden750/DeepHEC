271d4003e97871b61a725773763b2cd1008e6ea8
hayden750 <zdac052@live.rhul.ac.uk>
2021-04-26 10:24:24 +0100

    Major refactoring and cleaning repository

--- src/Replay_Buffer.py
+++ src/Replay_Buffer.py
@@ -59,12 +59,15 @@ class Buffer:
         return s_batch, a_batch, r_batch, ns_batch, d_batch
 
 
-class HER_Buffer:
+class HERBuffer:
     def __init__(self, buffer_capacity, batch_size):
         self.buffer_capacity = buffer_capacity
         self.batch_size = batch_size
         self.buffer = deque(maxlen=self.buffer_capacity)
         self.size = 0
+
+    def __len__(self):
+        return len(self.buffer)
 
     def record(self, state, action, reward, next_state, done, goal):
         if self.size <= self.buffer_capacity:
@@ -98,5 +101,22 @@ class HER_Buffer:
         goal_batch = tf.convert_to_tensor(goal_batch, dtype=tf.float32)
 
         return state_batch, action_batch, reward_batch, next_state_batch, done_batch, goal_batch
+
+    def get_all_samples(self):
+        s_batch = []
+        a_batch = []
+        r_batch = []
+        ns_batch = []
+        d_batch = []
+        g_batch = []
+        for i in range(len(self.buffer)):
+            s_batch.append(self.buffer[i][0])
+            a_batch.append(self.buffer[i][1])
+            r_batch.append(self.buffer[i][2])
+            ns_batch.append(self.buffer[i][3])
+            d_batch.append(self.buffer[i][4])
+            g_batch.append(self.buffer[i][5])
+
+        return s_batch, a_batch, r_batch, ns_batch, d_batch, g_batch
 
 
